cfc7bedf31df01c5a2b30a6333e740d28ce300e2
Saul Ramirez <fernando.ramire@hotmail.com>
2021-11-28 18:52:45 -0600

    :sparkles: adding schemas

---
+++ data/users.js
@@ -0,0 +1,6 @@
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '2', firstName: 'Sam', age: 20 },
+];
+
+module.exports = users;
---
+++ schema/schema.js
@@ -0,0 +1,37 @@
+const graphql = require('graphql');
+const users = require('../data/users');
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLID },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parentValue, args) => users.find((user) => user.id === args.id),
+    },
+  },
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery,
+  types: [UserType],
+});
+
+module.exports = schema;
