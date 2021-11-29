19e162f4c3041b21080525a6371dce5edc89b9fd
Saul Ramirez <fernando.ramire@hotmail.com>
2021-11-29 09:40:59 -0600

    🚀 adding json-server

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/graphql', expressGraphQL.graphqlHTTP({
 
 }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
